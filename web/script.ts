b8e005d9e75293bc8559a21d71df42de2fd24cbc
Riipah Furude <riipah@vocadb.net>
2016-10-08 17:51:30 +0300

    parsing

--- web/script.ts
+++ web/script.ts
@@ -10,33 +10,24 @@ class ViewModel {
 		if (match && match.length < 2)
 			return;
 
-		var url = match[1];
+		var pagename = match[1];
 
-		// http://utaulyrics.wikia.com/api.php?action=parse&disablepp=true&text=%7B%7B%23invoke%3Atest%20song%7Cshow%7C%E3%83%9B%E3%83%AF%E3%82%A4%E3%83%88%E3%83%8A%E3%82%A4%E3%83%88%20%28White%20Night%29%7D%7D&prop=text&format=json
-		var text = encodeURIComponent('{{#invoke:test song|show|') + url + encodeURIComponent('}}');
-		$.get("http://vocaloid.eu/utaulyricswiki/api/lyrics-wiki?pagename=" + url, null, result => {
+		$.get("http://vocaloid.eu/utaulyricswiki/api/lyrics-wiki?pagename=" + pagename, null, result => {
 
 			if (!result)
 				return;
 
-			var parsed = JSON.parse(result);
+			var parsed: LyricsWikiResult = JSON.parse(result);
 
-			this.originalTitle(parsed.names && parsed.names.original ? parsed.names.original : null);
-			this.media(parsed.media);
-			this.result(result);
+			this.result(parsed);
 
 		});
 
 	}
 
-	public media = ko.observableArray();
-
-	public result = ko.observable("");
-
-	public originalTitle = ko.observable("");
+	public result = ko.observable<LyricsWikiResult>(null);
 
 	public url = ko.observable("");
-
 
 }
 
--- web/typings/utaulyrics.d.ts
+++ web/typings/utaulyrics.d.ts
@@ -1,8 +1,29 @@
 interface LyricsWikiResult {
-	
+	lyrics: Lyrics;
+	media: Media[];
+	producers: Artist[];
+	vocalists: Artist[];	
+	names: Names;
 }
 
 interface Lyrics {
 	"ja-romanized"?: string;
 	"ja"?: string;
+}
+
+interface Media {
+	id: string;
+	website: "yt" | "nn" | "pp";
+}
+
+interface Artist {
+	vocadb?: string;
+	roles: string[];
+	name: string;
+}
+
+interface Names {
+	romanized?: string;
+	original: string;
+	english?: string;
 }
